dea62674fbaeb420958ef826548a994934c994ab
KOBAJIbCKUI <andvink@gmail.com>
2023-04-16 13:17:40 +0300

    Added utils for messaging

--- src/main/java/org/shootingcombats/shootingcombats/ShootingCombats.java
+++ src/main/java/org/shootingcombats/shootingcombats/ShootingCombats.java
@@ -1,17 +1,28 @@
 package org.shootingcombats.shootingcombats;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.shootingcombats.shootingcombats.util.Util;
 
 public final class ShootingCombats extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        if (!Util.isRunningMinecraftVersion(1, 12)) {
+            if (!Util.isRunningMinecraftVersion(1, 12, 2)) {
+                Util.warning("Shooting fights supports only version 1.12.2!");
+                Bukkit.getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
+        if (getDescription().getVersion().toLowerCase().contains("alpha")) {
+            Util.log("YOU ARE RUNNING AN ALPHA VERSION, so a lot of issues and bugs may appear during use!");
+        }
+        Util.log("Plugin enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Util.log("Plugin disabled");
     }
 }
