cb292ab18cf61f78f031746ee0c13ef717ab3218
KOBAJIbCKUI <andvink@gmail.com>
2023-04-20 23:31:15 +0300

    Add MapsConfig and rename Config to PluginConfig
    Add maps marshalling and unmarshalling with yaml configuration and rename Config to PluginConfig. Also add changes into PluginConfig

---
+++ src/main/java/org/shootingcombats/shootingcombats/data/MapsConfig.java
@@ -0,0 +1,147 @@
+package org.shootingcombats.shootingcombats.data;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.shootingcombats.shootingcombats.*;
+import org.shootingcombats.shootingcombats.util.Util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MapsConfig {
+    private final CombatMapManager mapsManager;
+    private FileConfiguration mapsConfig;
+    private Path configPath;
+
+    public MapsConfig() {
+        this.mapsManager = ShootingCombats.getMapsManager();
+        loadConfig();
+    }
+
+    public void loadConfig() {
+        if (this.configPath == null) {
+            this.configPath = Paths.get(ShootingCombats.getPlugin().getDataFolder().getPath(), "maps.yml");
+        }
+        if (!Files.exists(configPath)) {
+            try {
+                Files.createFile(configPath);
+                this.mapsConfig = YamlConfiguration.loadConfiguration(configPath.toFile());
+                saveConfig();
+                Util.log("New maps.yml has been successfully created!");
+            } catch (IOException e) {
+                Util.warning("Unable to create maps.yml!");
+                Util.debug(e);
+            }
+        } else {
+            this.mapsConfig = YamlConfiguration.loadConfiguration(configPath.toFile());
+            loadFromFile();
+            Util.log("maps.yml has been successfully loaded!");
+        }
+    }
+
+    public void saveConfig() {
+        try {
+            mapsConfig.save(configPath.toFile());
+        } catch (IOException e) {
+            Util.warning("Unable to save maps to " + configPath);
+            Util.debug(e);
+        }
+    }
+
+    public void saveToFile() {
+        Util.log("Saving maps...");
+        ConfigurationSection mapsSection = mapsConfig.createSection("maps");
+        ConfigurationSection mapSection, boundsSection, boundCornerSection, spawnPointsSection, spawnPointSection;
+        Bound bound;
+        Location corner;
+
+        for (CombatMap combatMap : ShootingCombats.getMapsManager().getMaps().keySet()) {
+            mapSection = mapsSection.createSection(combatMap.getName());
+
+            boundsSection = mapSection.createSection("bound");
+            bound = combatMap.getBound();
+            boundsSection.set("world", bound.getWorld());
+
+            boundCornerSection = boundsSection.createSection("greater_corner");
+            corner = combatMap.getBound().getGreaterCorner();
+            boundCornerSection.set("x", corner.getBlockX());
+            boundCornerSection.set("y", corner.getBlockY());
+            boundCornerSection.set("z", corner.getBlockZ());
+
+            boundCornerSection = boundsSection.createSection("lower_corner");
+            corner = combatMap.getBound().getLowerCorner();
+            boundCornerSection.set("x", corner.getBlockX());
+            boundCornerSection.set("y", corner.getBlockY());
+            boundCornerSection.set("z", corner.getBlockZ());
+
+            if (combatMap.spawnsNumber() == 0) {
+                continue;
+            }
+
+            spawnPointsSection = mapSection.createSection("spawn_points");
+
+            int index = 1;
+            for (Location location : combatMap.getSpawns()) {
+                spawnPointSection = spawnPointsSection.createSection(String.valueOf(index));
+                spawnPointSection.set("x", location.getBlockX());
+                spawnPointSection.set("y", location.getBlockY());
+                spawnPointSection.set("z", location.getBlockZ());
+                index++;
+            }
+        }
+        saveConfig();
+        Util.log("Maps successfully saved!");
+    }
+
+    public void loadFromFile() {
+        Util.log("Loading maps...");
+
+        if (!Files.exists(configPath)) {
+            Util.log("No maps to load");
+            return;
+        }
+
+        ConfigurationSection mapsSection = mapsConfig.getConfigurationSection("maps");
+
+        if (mapsSection != null) {
+            ConfigurationSection mapSection, boundsSection, boundCornerSection, spawnPointsSection, spawnPointSection;
+            String world;
+            Location greaterCorner, lowerCorner;
+            CombatMap combatMap;
+
+            for (String mapName : mapsSection.getKeys(false)) {
+                try {
+                    mapSection = mapsSection.getConfigurationSection(mapName);
+
+                    boundsSection = mapSection.getConfigurationSection("bound");
+                    world = boundsSection.getString("world");
+
+                    boundCornerSection = boundsSection.getConfigurationSection("greater_corner");
+                    greaterCorner = new Location(Bukkit.getWorld(world), boundCornerSection.getInt("x"), boundCornerSection.getInt("y"), boundCornerSection.getInt("z"));
+
+                    boundCornerSection = boundsSection.getConfigurationSection("lower_corner");
+                    lowerCorner = new Location(Bukkit.getWorld(world), boundCornerSection.getInt("x"), boundCornerSection.getInt("y"), boundCornerSection.getInt("z"));
+
+                    combatMap = new SimpleCombatMap(mapName, new SimpleBound(greaterCorner, lowerCorner));
+
+                    spawnPointsSection = mapSection.getConfigurationSection("spawn_points");
+
+                    for (String index : spawnPointsSection.getKeys(false)) {
+                        spawnPointSection = spawnPointsSection.getConfigurationSection(index);
+                        combatMap.addSpawn(new Location(Bukkit.getWorld(world), spawnPointSection.getInt("x"), spawnPointSection.getInt("y"), spawnPointSection.getInt("z")));
+                    }
+                    mapsManager.addMap(combatMap);
+                    Util.log("Map " + mapName + " has been successfully loaded");
+                } catch (NullPointerException e) {
+                    Util.log("Unable to load map " + mapName);
+                    Util.debug(e);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/org/shootingcombats/shootingcombats/data/PluginConfig.java
@@ -0,0 +1,147 @@
+package org.shootingcombats.shootingcombats.data;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.shootingcombats.shootingcombats.ShootingCombats;
+import org.shootingcombats.shootingcombats.util.Util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class PluginConfig {
+    private FileConfiguration pluginConfig;
+    private Path configPath;
+
+    //Global settings
+    public static int gbMaxLobbiesNumber;
+    public static int gbMaxMapsNumber;
+    public static int gbMaxRewardSize;
+    public static int gbMaxCombatDurMinutes;
+    public static boolean gbDebug;
+
+    //Deathmatch settings
+    public static int dmCombatDurMinutes;
+    public static boolean dmFinalTeleport;
+    public static int dmMinutesToFinalTp;
+    public static boolean dmEndgameTags;
+    public static int dmMinutesToTags;
+    public static boolean dmDeathSpectate;
+    public static int dmWinReward;
+    public static int dmLooseReward;
+    public static int dmKillReward;
+    //public static int dmKillstreakLength;
+    public static int dmBonus;
+
+    public PluginConfig() {
+        loadConfig();
+    }
+
+    public void loadConfig() {
+        if (this.configPath == null) {
+            this.configPath = Paths.get(ShootingCombats.getPlugin().getDataFolder().getPath(), "config.yml");
+        }
+        if (!Files.exists(configPath)) {
+            ShootingCombats.getPlugin().saveResource("config.yml", false);
+            Util.log("New config.yml has been successfully created!");
+        }
+        pluginConfig = YamlConfiguration.loadConfiguration(configPath.toFile());
+        loadConfigFromFile();
+        Util.log("config.yml has been successfully loaded!");
+    }
+
+    public void saveConfig() {
+        try {
+            pluginConfig.save(configPath.toFile());
+        } catch (IOException e) {
+            Util.warning("Unable to save configurations to " + configPath);
+            Util.debug(e);
+        }
+    }
+
+    public void loadConfigFromFile() {
+
+        String currentSection = "global."; //Global
+
+        gbDebug = pluginConfig.getBoolean(currentSection + "debug");
+
+        gbMaxLobbiesNumber = pluginConfig.getInt(currentSection + "max_lobbies_number");
+        gbMaxLobbiesNumber = gbMaxLobbiesNumber < 0 ? 8 : gbMaxLobbiesNumber;
+
+        gbMaxMapsNumber = pluginConfig.getInt(currentSection + "max_maps_number");
+        gbMaxMapsNumber = gbMaxMapsNumber < 0 ? 16 : gbMaxMapsNumber;
+
+        gbMaxRewardSize = pluginConfig.getInt(currentSection + "max_reward_size");
+        gbMaxRewardSize = gbMaxRewardSize < 0 || gbMaxRewardSize > 64 ? 64 : gbMaxRewardSize;
+
+        gbMaxCombatDurMinutes = pluginConfig.getInt(currentSection + "max_combat_duration_minutes");
+        gbMaxCombatDurMinutes = gbMaxCombatDurMinutes < 0 || gbMaxCombatDurMinutes > 30 ? 15 : gbMaxCombatDurMinutes;
+
+        currentSection = currentSection.replace("global.", "deathmatch.default_properties."); //Deathmatch default_properties
+
+        dmCombatDurMinutes = pluginConfig.getInt(currentSection + "combat_duration_minutes");
+        dmCombatDurMinutes = dmCombatDurMinutes < 1 || dmCombatDurMinutes > gbMaxCombatDurMinutes ? 15 : dmCombatDurMinutes;
+
+        dmMinutesToTags = pluginConfig.getInt(currentSection + "minutes_to_tags");
+        dmMinutesToTags = Math.min(dmMinutesToTags, dmCombatDurMinutes);
+
+        dmMinutesToFinalTp = pluginConfig.getInt(currentSection + "minutes_to_final_tp");
+        dmMinutesToFinalTp = Math.min(dmMinutesToFinalTp, dmCombatDurMinutes);
+
+        dmDeathSpectate = pluginConfig.getBoolean(currentSection + "spectate_after_death");
+        dmEndgameTags = pluginConfig.getBoolean(currentSection + "endgame_tags");
+        dmFinalTeleport = pluginConfig.getBoolean(currentSection + "final_teleport");
+
+        currentSection = currentSection.replace("default_properties.", "rewards."); //Deathmatch rewards
+
+        dmWinReward = pluginConfig.getInt(currentSection + "win");
+        dmWinReward = dmWinReward < 1 || dmWinReward > gbMaxRewardSize ? Math.min(dmWinReward, gbMaxRewardSize) : dmWinReward;
+
+        dmLooseReward = pluginConfig.getInt(currentSection + "loose");
+        dmLooseReward = dmLooseReward < 1 || dmLooseReward > gbMaxRewardSize ? Math.min(dmLooseReward, gbMaxRewardSize) : dmLooseReward;
+
+        dmKillReward = pluginConfig.getInt(currentSection + "kill");
+        dmKillReward = dmKillReward < 1 || dmKillReward > gbMaxRewardSize ? Math.min(dmKillReward, gbMaxRewardSize) : dmKillReward;
+
+        dmBonus = pluginConfig.getInt("bonus");
+        dmBonus = dmBonus < 1 || dmBonus > gbMaxRewardSize ? Math.min(dmBonus, gbMaxRewardSize) : dmBonus;
+        //dmKillstreakLength = config.getInt("killstreak_length", 3);
+
+        //currentSection = currentSection.replace("deathmatch.rewards.", ""); //Deathmatch rewards
+    }
+
+    public void saveConfigToFile() {
+        ConfigurationSection mainSection, subSection;
+
+        String currentSection = "global."; //Global
+
+        pluginConfig.set(currentSection + "debug", gbDebug);
+        pluginConfig.set(currentSection + "max_lobbies_number", gbMaxLobbiesNumber);
+        pluginConfig.set(currentSection + "max_maps_number", gbMaxMapsNumber);
+        pluginConfig.set(currentSection + "max_reward_size", gbMaxRewardSize);
+        pluginConfig.set(currentSection + "max_combat_duration_minutes", gbMaxCombatDurMinutes);
+
+        currentSection = currentSection.replace("global.", "deathmatch.default_properties."); //Deathmatch default properties
+
+        pluginConfig.set(currentSection + "combat_duration_minutes", dmCombatDurMinutes);
+        pluginConfig.set(currentSection + "endgame_tags", dmEndgameTags);
+        pluginConfig.set(currentSection + "minutes_to_tags", dmMinutesToTags);
+        pluginConfig.set(currentSection + "final_teleport", dmFinalTeleport);
+        pluginConfig.set(currentSection + "minutes_to_final_tp", dmMinutesToFinalTp);
+        pluginConfig.set(currentSection + "spectate_after_death", dmDeathSpectate);
+
+        //Rewards from config
+        currentSection = currentSection.replace("default_properties.", "rewards."); //Deathmatch rewards
+
+        pluginConfig.set(currentSection + "win",dmWinReward);
+        pluginConfig.set(currentSection + "loose", dmLooseReward);
+        pluginConfig.set(currentSection + "kill", dmKillReward);
+        pluginConfig.set(currentSection + "bonus", dmBonus);
+        //config.set("killstreak_length", killstreakLength);
+
+        saveConfig();
+    }
+
+}
